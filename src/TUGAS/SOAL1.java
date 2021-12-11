22dc4c9e879ba0b8944b1f32d3bae5fef9357eba
yaqinzz <ahmadainulyaqin131@gmail.com>
2021-12-11 19:16:02 +0700

    tugas

--- src/TUGAS/SOAL1.java
+++ src/TUGAS/SOAL1.java
@@ -1,5 +1,15 @@
 package TUGAS;
 
+import javax.swing.JOptionPane;
+
 public class SOAL1 {
-    
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("masukkan angka 1"));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("masukkan angka 2"));
+        if (n1 > n2) {
+            JOptionPane.showMessageDialog(null, "angka " + n1 + " adalah angka terbesar");
+        } else {
+            JOptionPane.showMessageDialog(null, "angka " + n2 + " adalah angka terbesar");
+        }
+    }
 }
